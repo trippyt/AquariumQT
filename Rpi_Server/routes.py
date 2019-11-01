c3d10cd4b1af3b1ac86cc789cae331defa56bbf5
trippyt <travisgleeson.94@gmail.com>
2019-11-01 14:33:32 +0000

    changing the temperature alerts function

--- Rpi_Server/routes.py
+++ Rpi_Server/routes.py
@@ -75,7 +75,8 @@ async def temp():
     while True:
         temp = await utils.temp()
         #print(temp)
-        await asyncio.sleep(2)
+        #await asyncio.sleep(2)
+        sleep(2)
         await websocket.send(str(temp))
 
 
